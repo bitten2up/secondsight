d1d6c3edc2d12ce1ca42749da0839fa2ddb13adb
bitten2up <seant9140@gmail.com>
2023-04-16 17:37:19 -0500

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+use std::io::{self, Write};
+
 pub fn secondsightify(t: &str) -> String {
     if t.chars().any(|x| (0xe0000 < (x as u32) && (x as u32) < 0xe007f)) {
-        println!("3y3 text detected. Revealing...");
+        eprintln!("3y3 text detected. Revealing...");
         return t.chars().map(|x| {
             if 0xe0000 < (x as u32) && (x as u32) < 0xe007f {
                 char::from_u32((x as u32) - 0xe0000).unwrap().to_string()
@@ -9,7 +11,7 @@ pub fn secondsightify(t: &str) -> String {
             }
         }).collect();
     } else {
-        println!("No 3y3 text detected. Concealing...");
+        eprintln!("No 3y3 text detected. Concealing...");
         return t.chars().map(|x| {
             if 0x00 < (x as u32) && (x as u32) < 0x7f {
                 char::from_u32((x as u32) + 0xe0000).unwrap().to_string()
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, Read};
 use secondsight;
+
 fn main() {
+
     // Read input from standard input
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).expect("failed to read from stdin");
